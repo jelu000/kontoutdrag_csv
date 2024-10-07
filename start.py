464640f2cadf38d43e0a06661dbea6733a8fa30a
Jens Lundeqvist <jelu000@gmail.com>
2024-10-07 23:01:18 +0200

    tredje commit

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 #läser fil
-df = pd.read_csv("Transaktioner_2024_jens_sept.csv")
-print(df.head())
+df = pd.read_csv("Transaktioner_2024_jens_sept.csv", skiprows=1, encoding="windows-1252")
+#print(df.head()) 
 
+totalt_belopp = df['Belopp'].sum()
+print(f"Totalt belopp: {totalt_belopp}")
