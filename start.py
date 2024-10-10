af96348a9b7af08bbf27f5ef294672f2184a9143
Jens <jelu000@gmail.com>
2024-10-10 23:54:19 +0200

    hämtade projekt till min andra dator

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #läser fil
 df = pd.read_csv("Transaktioner_2024_jens_sept.csv", skiprows=1, encoding="windows-1252")
-#print(df.head()) 
+print(df.head()) 
 
 totalt_belopp = df['Belopp'].sum()
 print(f"Totalt belopp: {totalt_belopp}")
