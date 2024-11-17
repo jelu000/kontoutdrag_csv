1778e478b9a847852fb89cd577badcc83f36f92b
Jens <jelu000@gmail.com>
2024-11-17 01:13:57 +0100

    Två menyer funkar i hanteraCsvKontoUtdrag

--- csvhandler.py
+++ csvhandler.py
@@ -1,6 +1,33 @@
 import duckdb
+import pandas as pd
 
 def print_csv_file(filename:str):
-    content=duckdb.read_csv(filename)
-    print(content)
-    klar=input("Nöjd?")
+    
+    try:
+        sqlfraga =f'SELECT * FROM "{filename}" ORDER BY Transdag ASC LIMIT 100'
+        content = duckdb.sql(sqlfraga).show(max_rows=200)        
+        print(content) 
+
+
+        #con = duckdb.connect()
+        # Visa alla rader
+        #con.execute("SET show_max_rows = 0")
+
+        # Läs in CSV-filen och visa resultatet
+        #content = con.execute(sqlfraga2).fetchall()
+        #print(content)
+
+        #df = pd.read_csv(filename, engine="python", on_bad_lines='skip', skiprows=1, nrows=200, encoding="utf-8")
+        #print(df.head())
+    except UnicodeError:
+        print("utf-8 fungerade inte, testar latin1...")
+        #df = pd.read_csv("konto_historik.csv", encoding="latin1")
+
+    klar=input("fortsätta? press enter")
+
+def print_csv_file_belopp(filename, belopp):
+    
+    sqlfraga1 = f'SELECT * FROM "{filename}" WHERE Belopp={belopp} ORDER BY Transdag ASC LIMIT 100'
+    content = duckdb.sql(sqlfraga1).show(max_rows=200)        
+    print(content) 
+    klar=input("fortsätta? press enter")
--- hanteraCsvKontoUtdrag.py
+++ hanteraCsvKontoUtdrag.py
@@ -2,19 +2,29 @@ import os
 import csvhandler
 
 def main():
-    year = input("Mata in år: ")
-    period = input("Mata in period 1 eller 2: ")
-    belopp = input("Mata in faktura belopp: ")
-    filnamn = input("Mata in namn på csv kontoutdrag: ")   
+    #year = input("Mata in år: ")
+    year = "2024"
+    #period = input("Mata in period 1 eller 2: ")
+    period="1"
+    #belopp = input("Mata in faktura belopp: ")
+    belopp="6000"
+    
+    #filnamn = input("Mata in namn på csv kontoutdrag: ")   
+    filnamn="test1.csv"
 
     while True:
         #Visa huvudmeny
-        val = huvudmenu()
+        val = huvudmenu(belopp, period, year)
 
         if val == "1":
-            print("1")
+            
+            csvhandler.print_csv_file_belopp(filnamn, belopp)
+        elif val == "2":#Avsluta
+            break
+
+        elif val == "3":#Avsluta
             csvhandler.print_csv_file(filnamn)
-        
+
         elif val == "9":#Avsluta
             break
 
@@ -22,17 +32,17 @@ def main():
             print("Ogiltig inmatning!")
 
 
-def huvudmenu():
+def huvudmenu(belopp, period, year):
     os.system('cls' if os.name == 'nt' else 'clear')
     
     print("\n------------------------------------------\n")
     print("\t-MENU- \U0001F603")
-    print("\t1. Visa betalningar från csv kontoutdrag")#order by datum
-    print("\t2. Föröver batalningar från csv kontoutdrag till DB")
-    print("\t3. Lista betalningar från Db för valt år och period")#order by datum
+    print(f"\t1. Visa inbetalningar på {belopp}kr från csv kontoutdrag sorterat på datum")#order by datum
+    print(f"\t2. Visa inbetalningar på {belopp}kr från csv kontoutdrag sorterat på husnr")#order by husnr
+    print("\t3. Visa hela kontoutdraget")#order by datum
     print("\t4. Lista årets betalningar för valt år")#Order by husnr
-    print("\t5. Lista fakturor")
-    print("\t6. Läggtill faktura")
+    print("\t5. Föröver batalningar från csv kontoutdrag till DB")
+    print("\t6. Lista betalningar från Db för valt år och period")
     print("\t7. Tabort faktura")
     print("\t8. Uppdatera faktura")
     print("\t9. Avsluta")
